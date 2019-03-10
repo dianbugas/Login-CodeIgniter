0db9c09798ab7629e292bc93413c43c8016fbf8c
dianynf <dianbugas@gmail.com>
2019-03-10 16:21:45 +0700

    form_validation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -18,9 +18,16 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        $data['title'] = 'WPU User Registration';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Nama', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'WPU User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'form berhasil di tambahkan';
+        }
     }
 }
