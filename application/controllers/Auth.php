0492ca53e5b7cdeb4ee9b2c531fbac571e5ea466
dianynf <dianbugas@gmail.com>
2019-03-10 16:40:45 +0700

    form_validation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -19,7 +19,14 @@ class Auth extends CI_Controller
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Nama', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email'); //|is_unique[user,email]
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
 
         if ($this->form_validation->run() == false) {
             $data['title'] = 'WPU User Registration';
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -11,18 +11,19 @@
               </div>
               <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
               <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="
-                  <?= set_value('name'); ?>">
+                  <input type="text" class="form-control form-control-user" id="name" name="name" value="
+                  <?= set_value('name'); ?>" placeholder="Full Name">
                   <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="
-                  <?= set_value('email'); ?>">
+                  <input type="text" class="form-control form-control-user" id="email" name="email" value="
+                  <?= set_value('email'); ?>" placeholder="Email Address">
                   <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                    <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                   </div>
                   <div class="col-sm-6">
                     <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
